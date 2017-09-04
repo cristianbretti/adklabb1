b5a95c621e367e9d010bd1581747800cfb6464cc
Cristian <cristian.osoriobretti@gmail.com>
2017-09-04 16:43:07 +0200

    Added a first java file with some basic methods for hashing

---
+++ Hash.java
@@ -0,0 +1,37 @@
+class Hash {
+
+	private static String alphabet = "\tabcdefghijklmnopqrstuvwxyzåäö";
+
+	public static void main(String[] args) {
+		String inputString = "abcfragra";
+		System.out.println(wordToIntHash(inputString));
+
+		for(int i = 0; i < 3; i++){
+			for(int j = 0; j < 30; j++){
+				for(int k = 0; k < 30; k++){
+
+					StringBuilder sb = new StringBuilder();
+					sb.append(alphabet.charAt(i));
+					sb.append(alphabet.charAt(j));
+					sb.append(alphabet.charAt(k));	
+
+					String currentString = sb.toString();				
+					int currentHash = wordToIntHash(currentString);
+
+					System.out.println(currentString + "  " + currentHash);
+				}
+			}
+		}
+
+	}
+
+
+	private static int wordToIntHash(String inputString){
+		String firstThree = inputString.substring(0,3);
+
+		return alphabet.indexOf(firstThree.charAt(0))*900 
+						+ alphabet.indexOf(firstThree.charAt(1))*30
+						 + alphabet.indexOf(firstThree.charAt(2));
+
+	}
+}
